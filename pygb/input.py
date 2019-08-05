69e3c3c4809126aac99d18450ead0d4fe02e56af
vitorsilverio <vitor.silverio.rodrigues@gmail.com>
2019-08-05 11:33:16 -0300

    rename input var to a no reserved word

--- pygb/input.py
+++ pygb/input.py
@@ -16,24 +16,24 @@ class Input:
         }
 
     def read_input(self, joyp):
-        input = 0x0f
+        _input = 0x0f
         if joyp & 0x20 == 0x00:
             if self.buttons['START']:
-                input ^= 0x08
+                _input ^= 0x08
             elif self.buttons['SELECT']:
-                input ^= 0x04
+                _input ^= 0x04
             elif self.buttons['A']:
-                input ^= 0x01
+                _input ^= 0x01
             elif self.buttons['B']:
-                input ^= 0x02
+                _input ^= 0x02
         elif joyp & 0x10 == 0x00:
             if self.buttons['UP']:
-                input ^= 0x04
+                _input ^= 0x04
             elif self.buttons['DOWN']:
-                input ^= 0x08
+                _input ^= 0x08
             elif self.buttons['LEFT']:
-                input ^= 0x02
+                _input ^= 0x02
             elif self.buttons['RIGHT']:
-                input ^= 0x01
+                _input ^= 0x01
 
-        return (0xf0 & joyp) | input
+        return (0xf0 & joyp) | _input
