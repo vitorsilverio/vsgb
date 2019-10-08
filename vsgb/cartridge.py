dcf51ad8524b750438c53449465d94f911ff2303
vitorsilverio <vitor.silverio.rodrigues@gmail.com>
2019-10-08 18:28:31 -0300

    Reimplemented MBC1 and added documentation

--- vsgb/cartridge.py
+++ vsgb/cartridge.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+# Implemented following https://gbdev.gg8.se/wiki/articles/Memory_Bank_Controllers instructions
+
 import logging
 import os
 import struct
@@ -104,7 +106,8 @@ class CartridgeType:
         self.ram = [0x00] * (0x2000 * self.ram_banks)
 
         if self.hasBattery:
-            self.battery = Battery('rom.sav')
+            save_file_name = bytes(self.data[0x0134:0x0143]).decode().rstrip('\x00')+".sav"
+            self.battery = Battery(save_file_name)
             self.battery.load_ram(self.ram)
 
 
@@ -125,92 +128,102 @@ class ROM(CartridgeType):
     def __init__(self, data: list, hasRam: bool, hasBattery: bool):
         super().__init__(data, hasRam, hasBattery)
 
+
+# MBC1 (max 2MByte ROM and/or 32KByte RAM)
+# ----------------------------------------
+# This is the first MBC chip for the gameboy. Any newer MBC chips are working similiar, 
+# so that is relative easy to upgrade a program from one MBC chip to another - or even 
+# to make it compatible to several different types of MBCs.
+# Note that the memory in range 0000-7FFF is used for both reading from ROM, and for 
+# writing to the MBCs Control Registers. 
 class MBC1(CartridgeType):
 
     def __init__(self, data: list, hasRam: bool, hasBattery: bool):
         super().__init__(data, hasRam, hasBattery)
         self.selected_ram_bank = 0
         self.selected_rom_bank = 1
-        self.memory_model = 0
-        self.cached_ram_bank_for_0x4000 = -1
-        self.ram_write_enabled = False
+        self.memory_mode = 0
+        self.ram_enabled = False
 
-    def get_rom_bank_for_0x0000(self):
-        return 0
-
-    def get_rom_bank_for_0x4000(self):
-        if self.cached_ram_bank_for_0x4000 == -1:
-            bank = self.selected_rom_bank
-            if bank % 0x20 == 0 or bank == 0:
-                bank += 1
-            if self.memory_model == 1:
-                bank &= 0b00011111
-                bank |= (self.selected_ram_bank << 5)
-            bank %= self.rom_banks
-            self.cached_ram_bank_for_0x4000 = bank
-        return self.cached_ram_bank_for_0x4000
-
-    def get_rom_byte(self, bank, address):
-        cartOffset = bank * 0x4000 + address
-        if cartOffset < len(self.data):
-            return self.data[cartOffset]
-        else:
-            return 0xff
-
-    def get_ram_address(self, address):
-        if self.memory_model == 0:
-            return address - 0xa000
-        else:
-            return self.selected_ram_bank * 0x2000 + (address - 0xa000)
-        
+      
     def read_rom_byte(self, address : int) -> int:
+        # 0000-3FFF - ROM Bank 00 (Read Only)
+        # This area always contains the first 16KBytes of the cartridge ROM.
         if address < 0x4000:
-            return self.get_rom_byte(self.get_rom_bank_for_0x0000(), address)
-        else:
-            return self.get_rom_byte(self.get_rom_bank_for_0x4000(), address - 0x4000)
+            return self.data[address]
+        # 4000-7FFF - ROM Bank 01-7F (Read Only)
+        # This area may contain any of the further 16KByte banks of the ROM, allowing to 
+        # address up to 125 ROM Banks (almost 2MByte). As described below, bank numbers 
+        # 20h, 40h, and 60h cannot be used, resulting in the odd amount of 125 banks. 
+        if self.selected_rom_bank in [0, 0x20, 0x40, 0x60]:
+            self.selected_rom_bank += 1
+        return self.data[(0x4000 * self.selected_rom_bank) + (address - 0x4000)]
 
     def write_rom_byte(self, address : int, value : int):
-        if address < 0x2000:
-            self.ram_write_enabled = (value & 0b1111) == 0b1010
-            if not self.ram_write_enabled:
-                self.battery.save_ram(self.ram)
-        elif 0x2000 <= address < 0x4000:
-            bank = self.selected_rom_bank & 0b01100000
-            bank = bank | (value & 0b00011111)
-            self.selected_rom_bank  = bank
-            self.cached_rom_bank_for_0x0000 = -1
-            self.cached_ram_bank_for_0x4000 = -1
-        elif 0x4000 <= address < 0x6000 and self.memory_model == 0:
-            bank = self.selected_rom_bank & 0b00011111
-            bank = bank | ((value & 0b11) << 5)
-            self.selected_rom_bank  = bank
-            self.cached_rom_bank_for_0x0000 = -1
-            self.cached_ram_bank_for_0x4000 = -1
-        elif 0x4000 <= address < 0x6000 and self.memory_model == 1:
-            bank = value & 0b11
-            self.selected_ram_bank = bank
-            self.cached_rom_bank_for_0x0000 = -1
-            self.cached_ram_bank_for_0x4000 = -1
-        elif 0x6000 <= address < 0x8000:
-            self.memory_model = value & 1
-            self.cached_rom_bank_for_0x0000 = -1
-            self.cached_ram_bank_for_0x4000 = -1
+        # 0000-1FFF - RAM Enable (Write Only)
+        # Before external RAM can be read or written, it must be enabled by writing to this 
+        # address space. It is recommended to disable external RAM after accessing it, in 
+        # order to protect its contents from damage during power down of the gameboy. Usually 
+        # the following values are used:
+        # - 00h  Disable RAM (default)
+        # - 0Ah  Enable RAM
+        # Practically any value with 0Ah in the lower 4 bits enables RAM, and any other value disables RAM.
+        if 0x0000 <= address <= 0x1fff:
+            self.ram_enabled = (value & 0x0f == 0x0a)
+
+        # 2000-3FFF - ROM Bank Number (Write Only)
+        # Writing to this address space selects the lower 5 bits of the ROM Bank Number (in range 01-1Fh). 
+        # When 00h is written, the MBC translates that to bank 01h also. That doesn't harm so far, because 
+        # ROM Bank 00h can be always directly accessed by reading from 0000-3FFF. But (when using the register 
+        # below to specify the upper ROM Bank bits), the same happens for Bank 20h, 40h, and 60h. Any attempt 
+        # to address these ROM Banks will select Bank 21h, 41h, and 61h instead. 
+        if 0x2000 <= address <= 0x3fff:
+            self.selected_rom_bank = value & 0b00011111
+            if self.selected_rom_bank in [0, 0x20, 0x40, 0x60]:
+                self.selected_rom_bank += 1
+
+        # 4000-5FFF - RAM Bank Number - or - Upper Bits of ROM Bank Number (Write Only)
+        # This 2bit register can be used to select a RAM Bank in range from 00-03h, or to specify the upper 
+        # two bits (Bit 5-6) of the ROM Bank number, depending on the current ROM/RAM Mode. (See below.) 
+        if 0x4000 <= address <= 0x5fff:
+            if self.memory_mode == 0:
+                self.selected_ram_bank = (value & 0b00000011)
+                self.selected_rom_bank = self.selected_rom_bank & 0b00011111
+            else:
+                self.selected_ram_bank = 0
+                self.selected_rom_bank = self.selected_rom_bank | ((value & 0b00000011) << 5)
+        
+        # 6000-7FFF - ROM/RAM Mode Select (Write Only)
+        # This 1bit Register selects whether the two bits of the above register should be used as upper 
+        # two bits of the ROM Bank, or as RAM Bank Number. 
+        # - 00h = ROM Banking Mode (up to 8KByte RAM, 2MByte ROM) (default)
+        # - 01h = RAM Banking Mode (up to 32KByte RAM, 512KByte ROM)
+        # The program may freely switch between both modes, the only limitiation is that only RAM Bank 00h 
+        # can be used during Mode 0, and only ROM Banks 00-1Fh can be used during Mode 1.
+        if 0x6000 <= address <= 0x7fff:
+            self.memory_mode = value & 0b00000001
         
     def read_external_ram_byte(self, address : int) -> int:
-        if self.ram_write_enabled:
-            ram_address = self.get_ram_address(address)
-            if ram_address < len(self.ram):
-                return self.ram[ram_address]
-            else:
-                return 0xff
+        # A000-BFFF - RAM Bank 00-03, if any (Read/Write)
+        # This area is used to address external RAM in the cartridge (if any). External RAM is often battery 
+        # buffered, allowing to store game positions or high score tables, even if the gameboy is turned off, 
+        # or if the cartridge is removed from the gameboy. Available RAM sizes are: 2KByte (at A000-A7FF), 
+        # 8KByte (at A000-BFFF), and 32KByte (in form of four 8K banks at A000-BFFF). 
+        if self.ram_enabled:
+            return self.ram[(self.selected_ram_bank * 0x2000) + (address - 0xa000)]
         else:
-            return 0xff
+            0xff
 
     def write_external_ram_byte(self, address : int, value : int):
-        if self.ram_write_enabled:
-            ram_address = self.get_ram_address(address)
-            if ram_address < len(self.ram):
-                self.ram[ram_address] = value
+        # A000-BFFF - RAM Bank 00-03, if any (Read/Write)
+        # This area is used to address external RAM in the cartridge (if any). External RAM is often battery 
+        # buffered, allowing to store game positions or high score tables, even if the gameboy is turned off, 
+        # or if the cartridge is removed from the gameboy. Available RAM sizes are: 2KByte (at A000-A7FF), 
+        # 8KByte (at A000-BFFF), and 32KByte (in form of four 8K banks at A000-BFFF). 
+        if self.ram_enabled:
+            self.ram[(self.selected_ram_bank * 0x2000) + (address - 0xa000)] = value
+            if self.hasBattery:
+                self.battery.save_ram(self.ram)
         
 
 class MBC2(CartridgeType):
@@ -223,188 +236,10 @@ class MBC3(CartridgeType):
 
     def __init__(self, data: list, hasRam: bool, hasBattery: bool, hasTimer: bool):
         super().__init__(data, hasRam, hasBattery)
-        self.selected_ram_bank = 0
-        self.selected_rom_bank = 1
-        self.memory_model = 0
-        self.cached_rom_bank_for_0x0000 = -1
-        self.cached_ram_bank_for_0x4000 = -1
-        self.ram_write_enabled = False
-
-    def get_rom_bank_for_0x0000(self):
-        if self.cached_rom_bank_for_0x0000 == -1:
-            if self.memory_model == 0:
-                self.cached_rom_bank_for_0x0000 = 0
-            else:
-                bank = self.selected_ram_bank << 5
-                bank %= self.rom_banks
-                self.cached_rom_bank_for_0x0000 = bank
-        return self.cached_rom_bank_for_0x0000
-
-    def get_rom_bank_for_0x4000(self):
-        if self.cached_ram_bank_for_0x4000 == -1:
-            bank = self.selected_rom_bank
-            if bank % 0x20 == 0:
-                bank += 1
-            if self.memory_model == 1:
-                bank &= 0b00011111
-                bank |= (self.selected_ram_bank << 5)
-            bank %= self.rom_banks
-            self.cached_ram_bank_for_0x4000 = bank
-        return self.cached_ram_bank_for_0x4000
-
-    def get_rom_byte(self, bank, address):
-        cartOffset = bank * 0x4000 + address
-        if cartOffset < len(self.data):
-            return self.data[cartOffset]
-        else:
-            return 0xff
-
-    def get_ram_address(self, address):
-        if self.memory_model == 0:
-            return address - 0xa000
-        else:
-            return (self.selected_ram_bank % self.ram_banks) * 0x2000 + (address - 0xa000)
-        
-    def read_rom_byte(self, address : int) -> int:
-        if address < 0x4000:
-            return self.get_rom_byte(self.get_rom_bank_for_0x0000(), address)
-        else:
-            return self.get_rom_byte(self.get_rom_bank_for_0x4000(), address - 0x4000)
-
-    def write_rom_byte(self, address : int, value : int):
-        if address < 0x2000:
-            self.ram_write_enabled = (value & 0b1111) == 0b1010
-            if not self.ram_write_enabled:
-                self.battery.save_ram(self.ram)
-        elif 0x2000 <= address < 0x4000:
-            bank = self.selected_rom_bank & 0b01100000
-            bank = bank | (value & 0b00011111)
-            self.selected_rom_bank  = bank
-            self.cached_rom_bank_for_0x0000 = -1
-            self.cached_ram_bank_for_0x4000 = -1
-        elif 0x4000 <= address < 0x6000 and self.memory_model == 0:
-            bank = self.selected_rom_bank & 0b00011111
-            bank = bank | ((value & 0b11) << 5)
-            self.selected_rom_bank  = bank
-            self.cached_rom_bank_for_0x0000 = -1
-            self.cached_ram_bank_for_0x4000 = -1
-        elif 0x4000 <= address < 0x6000 and self.memory_model == 1:
-            bank = value & 0b11
-            self.selected_ram_bank = bank
-            self.cached_rom_bank_for_0x0000 = -1
-            self.cached_ram_bank_for_0x4000 = -1
-        elif 0x6000 <= address < 0x8000:
-            self.memory_model = value & 1
-            self.cached_rom_bank_for_0x0000 = -1
-            self.cached_ram_bank_for_0x4000 = -1
-        
-    def read_external_ram_byte(self, address : int) -> int:
-        if self.ram_write_enabled:
-            ram_address = self.get_ram_address(address)
-            if ram_address < len(self.ram):
-                return self.ram[ram_address]
-            else:
-                return 0xff
-        else:
-            return 0xff
-
-    def write_external_ram_byte(self, address : int, value : int):
-        if self.ram_write_enabled:
-            ram_address = self.get_ram_address(address)
-            if ram_address < len(self.ram):
-                self.ram[ram_address] = value
+        logging.warning('MBC3 is not implemented')
 
 class MBC5(CartridgeType):
 
     def __init__(self, data: list, hasRam: bool, hasBattery: bool):
         super().__init__(data, hasRam, hasBattery)
-        self.selected_ram_bank = 0
-        self.selected_rom_bank = 1
-        self.memory_model = 0
-        self.cached_rom_bank_for_0x0000 = -1
-        self.cached_ram_bank_for_0x4000 = -1
-        self.ram_write_enabled = False
-
-    def get_rom_bank_for_0x0000(self):
-        if self.cached_rom_bank_for_0x0000 == -1:
-            if self.memory_model == 0:
-                self.cached_rom_bank_for_0x0000 = 0
-            else:
-                bank = self.selected_ram_bank << 5
-                bank %= self.rom_banks
-                self.cached_rom_bank_for_0x0000 = bank
-        return self.cached_rom_bank_for_0x0000
-
-    def get_rom_bank_for_0x4000(self):
-        if self.cached_ram_bank_for_0x4000 == -1:
-            bank = self.selected_rom_bank
-            if bank % 0x20 == 0:
-                bank += 1
-            if self.memory_model == 1:
-                bank &= 0b00011111
-                bank |= (self.selected_ram_bank << 5)
-            bank %= self.rom_banks
-            self.cached_ram_bank_for_0x4000 = bank
-        return self.cached_ram_bank_for_0x4000
-
-    def get_rom_byte(self, bank, address):
-        cartOffset = bank * 0x4000 + address
-        if cartOffset < len(self.data):
-            return self.data[cartOffset]
-        else:
-            return 0xff
-
-    def get_ram_address(self, address):
-        if self.memory_model == 0:
-            return address - 0xa000
-        else:
-            return (self.selected_ram_bank % self.ram_banks) * 0x2000 + (address - 0xa000)
-        
-    def read_rom_byte(self, address : int) -> int:
-        if address < 0x4000:
-            return self.get_rom_byte(self.get_rom_bank_for_0x0000(), address)
-        else:
-            return self.get_rom_byte(self.get_rom_bank_for_0x4000(), address - 0x4000)
-
-    def write_rom_byte(self, address : int, value : int):
-        if address < 0x2000:
-            self.ram_write_enabled = (value & 0b1111) == 0b1010
-            if not self.ram_write_enabled:
-                self.battery.save_ram(self.ram)
-        elif 0x2000 <= address < 0x4000:
-            bank = self.selected_rom_bank & 0b01100000
-            bank = bank | (value & 0b00011111)
-            self.selected_rom_bank  = bank
-            self.cached_rom_bank_for_0x0000 = -1
-            self.cached_ram_bank_for_0x4000 = -1
-        elif 0x4000 <= address < 0x6000 and self.memory_model == 0:
-            bank = self.selected_rom_bank & 0b00011111
-            bank = bank | ((value & 0b11) << 5)
-            self.selected_rom_bank  = bank
-            self.cached_rom_bank_for_0x0000 = -1
-            self.cached_ram_bank_for_0x4000 = -1
-        elif 0x4000 <= address < 0x6000 and self.memory_model == 1:
-            bank = value & 0b11
-            self.selected_ram_bank = bank
-            self.cached_rom_bank_for_0x0000 = -1
-            self.cached_ram_bank_for_0x4000 = -1
-        elif 0x6000 <= address < 0x8000:
-            self.memory_model = value & 1
-            self.cached_rom_bank_for_0x0000 = -1
-            self.cached_ram_bank_for_0x4000 = -1
-        
-    def read_external_ram_byte(self, address : int) -> int:
-        if self.ram_write_enabled:
-            ram_address = self.get_ram_address(address)
-            if ram_address < len(self.ram):
-                return self.ram[ram_address]
-            else:
-                return 0xff
-        else:
-            return 0xff
-
-    def write_external_ram_byte(self, address : int, value : int):
-        if self.ram_write_enabled:
-            ram_address = self.get_ram_address(address)
-            if ram_address < len(self.ram):
-                self.ram[ram_address] = value
+        logging.warning('MBC5 is not implemented')
