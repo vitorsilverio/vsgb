083b63ff97f8ffdf76c42cc02dcfcaa79d0c0587
Vitor S Rodrigues <vitor.silverio.rodrigues@gmail.com>
2019-10-09 09:50:24 -0300

    add docs

--- vsgb/cartridge.py
+++ vsgb/cartridge.py
@@ -1,11 +1,149 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-# Implemented following https://gbdev.gg8.se/wiki/articles/Memory_Bank_Controllers instructions
+# Documentation source:
+# - https://gbdev.gg8.se/wiki/articles/The_Cartridge_Header
+# - https://gbdev.gg8.se/wiki/articles/Memory_Bank_Controllers
 
 import logging
 import os
 import struct
+
+class CartridgeHeader:
+    # 0100-0103 - Entry Point
+    # After displaying the Nintendo Logo, the built-in boot procedure jumps to this address (100h), 
+    # which should then jump to the actual main program in the cartridge. Usually this 4 byte area 
+    # contains a NOP instruction, followed by a JP 0150h instruction. But not always. 
+    ENTRY_POINT = 0x0100
+
+    # 0104-0133 - Nintendo Logo
+    # These bytes define the bitmap of the Nintendo logo that is displayed when the gameboy gets 
+    # turned on. The hexdump of this bitmap is:
+    #     CE ED 66 66 CC 0D 00 0B 03 73 00 83 00 0C 00 0D
+    #     00 08 11 1F 88 89 00 0E DC CC 6E E6 DD DD D9 99
+    #     BB BB 67 63 6E 0E EC CC DD DC 99 9F BB B9 33 3E
+    # The Game Boy's boot procedure verifies the content of this bitmap (after it has displayed it),
+    # and LOCKS ITSELF UP if these bytes are incorrect. A CGB verifies only the first 18h bytes of 
+    # the bitmap, but others (for example a pocket gameboy) verify all 30h bytes. 
+    NINTENDO_LOGO = 0x0104
+    
+    # 0134-0143 - Title
+    # Title of the game in UPPER CASE ASCII. If it is less than 16 characters then the remaining bytes 
+    # are filled with 00's. When inventing the CGB, Nintendo has reduced the length of this area to 
+    # 15 characters, and some months later they had the fantastic idea to reduce it to 11 characters 
+    # only. The new meaning of the ex-title bytes is described below. 
+    TITLE = 0x0134
+
+    # 013F-0142 - Manufacturer Code
+    # In older cartridges this area has been part of the Title (see above), in newer cartridges this 
+    # area contains an 4 character uppercase manufacturer code. Purpose and Deeper Meaning unknown. 
+    MANUFACTURER_CODE = 0x013f
+
+    # 0143 - CGB Flag
+    # In older cartridges this byte has been part of the Title (see above). In CGB cartridges the 
+    # upper bit is used to enable CGB functions. This is required, otherwise the CGB switches itself 
+    # into Non-CGB-Mode. Typical values are:
+    #   80h - Game supports CGB functions, but works on old gameboys also.
+    #   C0h - Game works on CGB only (physically the same as 80h).
+    # Values with Bit 7 set, and either Bit 2 or 3 set, will switch the gameboy into a special 
+    # non-CGB-mode with uninitialized palettes. Purpose unknown, eventually this has been supposed 
+    # to be used to colorize monochrome games that include fixed palette data at a special location 
+    # in ROM. 
+    CGB_FLAG = 0x0143
+
+    # 0144-0145 - New Licensee Code
+    # Specifies a two character ASCII licensee code, indicating the company or publisher of the game. 
+    # These two bytes are used in newer games only (games that have been released after the SGB has 
+    # been invented). Older games are using the header entry at 014B instead. 
+    NEW_LICENSE_CODE = 0x0144
+
+    # 0146 - SGB Flag
+    # Specifies whether the game supports SGB functions, common values are: 
+    #   00h = No SGB functions (Normal Gameboy or CGB only game)
+    #   03h = Game supports SGB functions
+    # The SGB disables its SGB functions if this byte is set to another value than 03h. 
+    SGB_FLAG = 0x0146
+
+    # 0147 - Cartridge Type
+    # Specifies which Memory Bank Controller (if any) is used in the cartridge, and if further external 
+    # hardware exists in the cartridge. 
+    #  00h  ROM ONLY                 19h  MBC5
+    # 01h  MBC1                     1Ah  MBC5+RAM
+    # 02h  MBC1+RAM                 1Bh  MBC5+RAM+BATTERY
+    # 03h  MBC1+RAM+BATTERY         1Ch  MBC5+RUMBLE
+    # 05h  MBC2                     1Dh  MBC5+RUMBLE+RAM
+    # 06h  MBC2+BATTERY             1Eh  MBC5+RUMBLE+RAM+BATTERY
+    # 08h  ROM+RAM                  20h  MBC6
+    # 09h  ROM+RAM+BATTERY          22h  MBC7+SENSOR+RUMBLE+RAM+BATTERY
+    # 0Bh  MMM01
+    # 0Ch  MMM01+RAM
+    # 0Dh  MMM01+RAM+BATTERY
+    # 0Fh  MBC3+TIMER+BATTERY
+    # 10h  MBC3+TIMER+RAM+BATTERY   FCh  POCKET CAMERA
+    # 11h  MBC3                     FDh  BANDAI TAMA5
+    # 12h  MBC3+RAM                 FEh  HuC3
+    # 13h  MBC3+RAM+BATTERY         FFh  HuC1+RAM+BATTERY
+    CARTRIDGE_TYPE = 0x0147
+
+    # 0148 - ROM Size
+    # Specifies the ROM Size of the cartridge. Typically calculated as "32KB shl N". 
+    # 00h -  32KByte (no ROM banking)
+    # 01h -  64KByte (4 banks)
+    # 02h - 128KByte (8 banks)
+    # 03h - 256KByte (16 banks)
+    # 04h - 512KByte (32 banks)
+    # 05h -   1MByte (64 banks)  - only 63 banks used by MBC1
+    # 06h -   2MByte (128 banks) - only 125 banks used by MBC1
+    # 07h -   4MByte (256 banks)
+    # 08h -   8MByte (512 banks)
+    # 52h - 1.1MByte (72 banks)
+    # 53h - 1.2MByte (80 banks)
+    # 54h - 1.5MByte (96 banks)
+    ROM_SIZE = 0x0148
+
+    # 0149 - RAM Size
+    # Specifies the size of the external RAM in the cartridge (if any). 
+    # 00h - None
+    # 01h - 2 KBytes
+    # 02h - 8 Kbytes
+    # 03h - 32 KBytes (4 banks of 8KBytes each)
+    # 04h - 128 KBytes (16 banks of 8KBytes each)
+    # 05h - 64 KBytes (8 banks of 8KBytes each)
+    # When using a MBC2 chip 00h must be specified in this entry, even though the MBC2 includes a 
+    # built-in RAM of 512 x 4 bits.
+    RAM_SIZE = 0x0149
+
+    # 014A - Destination Code
+    # Specifies if this version of the game is supposed to be sold in Japan, or anywhere else. 
+    # Only two values are defined. 
+    # 00h - Japanese
+    # 01h - Non-Japanese
+    DESTINATION_CODE = 0x014a
+
+    # 014B - Old Licensee Code
+    # Specifies the games company/publisher code in range 00-FFh. A value of 33h signalizes 
+    # that the New License Code in header bytes 0144-0145 is used instead. (Super GameBoy 
+    # functions won't work if <> $33.) 
+    OLD_LICENSE_CODE = 0x014b
+    
+    # 014C - Mask ROM Version number
+    # Specifies the version number of the game. That is usually 00h. 
+    MASK_ROM_VERSION = 0x014c
+
+    # 014D - Header Checksum
+    # Contains an 8 bit checksum across the cartridge header bytes 0134-014C. The checksum is 
+    # calculated as follows:
+    #     x=0:FOR i=0134h TO 014Ch:x=x-MEM[i]-1:NEXT
+    # The lower 8 bits of the result must be the same than the value in this entry. The GAME 
+    # WON'T WORK if this checksum is incorrect. 
+    HEADER_CHECKSUM = 0x014d
+
+    # 014E-014F - Global Checksum
+    # Contains a 16 bit checksum (upper byte first) across the whole cartridge ROM. Produced 
+    # by adding all bytes of the cartridge (except for the two checksum bytes). The Gameboy 
+    # doesn't verify this checksum. 
+    GLOBAL_CHECKSUM = 0x014e
+
 
 class Cartridge:
 
@@ -17,7 +155,7 @@ class Cartridge:
                 self.data.append(struct.unpack('<B', f.read(1))[0])
 
     def rom(self):
-        rom_type = self.data[0x147]
+        rom_type = self.data[CartridgeHeader.CARTRIDGE_TYPE]
         if rom_type == 0x00:
             return ROM(self.data, False, False)
         if rom_type == 0x01:
@@ -80,7 +218,7 @@ class CartridgeType:
         self.hasRam = hasRam
         self.hasBattery = hasBattery
 
-        rom_banks_reg = self.data[0x0148]
+        rom_banks_reg = self.data[CartridgeHeader.ROM_SIZE]
         self.rom_banks = {
             0x00: 2,
             0x01: 4,
@@ -95,7 +233,7 @@ class CartridgeType:
             0x54: 96
         }.get(rom_banks_reg, 0)
 
-        ram_banks_reg = self.data[0x0149]
+        ram_banks_reg = self.data[CartridgeHeader.RAM_SIZE]
         self.ram_banks = {
             0x00: 0,
             0x01: 1,
@@ -107,7 +245,7 @@ class CartridgeType:
         self.ram = [0xff] * (0x2000 * self.ram_banks)
 
         if self.hasBattery:
-            save_file_name = bytes(self.data[0x0134:0x0143]).decode().rstrip('\x00')+".sav"
+            save_file_name = bytes(self.data[CartridgeHeader.TITLE:(CartridgeHeader.TITLE + 15)]).decode().rstrip('\x00')+".sav"
             self.battery = Battery(save_file_name)
             self.ram = self.battery.load_ram(self.ram)
 
@@ -124,6 +262,12 @@ class CartridgeType:
     def write_external_ram_byte(self, address : int, value : int):
         pass
 
+# None (32KByte ROM only)
+# Small games of not more than 32KBytes ROM do not require a MBC chip 
+# for ROM banking. The ROM is directly mapped to memory at 0000-7FFFh. 
+# Optionally up to 8KByte of RAM could be connected at A000-BFFF, even 
+# though that could require a tiny MBC-like circuit, but no real MBC 
+# chip. 
 class ROM(CartridgeType):
 
     def __init__(self, data: list, hasRam: bool, hasBattery: bool):
@@ -233,19 +377,25 @@ class MBC1(CartridgeType):
         if self.ram_enabled:
             self.ram[(self.ram_bank * 0x2000) + (address - 0xa000)] = value
         
-
+# MBC2 (max 256KByte ROM and 512x4 bits RAM)
 class MBC2(CartridgeType):
 
     def __init__(self, data: list, hasBattery: bool):
         super().__init__(data, False, hasBattery)
         logging.warning('MBC2 is not implemented')
 
+# MBC3 (max 2MByte ROM and/or 64KByte RAM and Timer)
+# Beside for the ability to access up to 2MB ROM (128 banks), and 64KB RAM (8 banks), the MBC3 also includes 
+# a built-in Real Time Clock (RTC). The RTC requires an external 32.768 kHz Quartz Oscillator, and an external 
+# battery (if it should continue to tick when the gameboy is turned off).
 class MBC3(MBC1):
 
     def __init__(self, data: list, hasRam: bool, hasBattery: bool, hasTimer: bool):
         super().__init__(data, hasRam, hasBattery)
         logging.warning('MBC3 is not implemented')
 
+
+# MBC5 (max 8MByte ROM and/or 128KByte RAM)
 class MBC5(CartridgeType):
 
     def __init__(self, data: list, hasRam: bool, hasBattery: bool):
