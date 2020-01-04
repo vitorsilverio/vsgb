449fcd17576777e4df1a953d1690e27b5f513e74
vitorsilverio <vitor.silverio.rodrigues@gmail.com>
2020-01-03 23:40:22 -0300

    default color values

--- vsgb/cgb_palette.py
+++ vsgb/cgb_palette.py
@@ -11,14 +11,14 @@ class CGB_Palette:
 
         # FF68 - BCPS/BGPI - CGB Mode Only - Background Palette Index
         self.bg_palettes = [
-            Color(),Color(),Color(),Color(), #BGP0
-            Color(),Color(),Color(),Color(), #BGP1
-            Color(),Color(),Color(),Color(), #BGP2
-            Color(),Color(),Color(),Color(), #BGP3
-            Color(),Color(),Color(),Color(), #BGP4
-            Color(),Color(),Color(),Color(), #BGP5
-            Color(),Color(),Color(),Color(), #BGP6
-            Color(),Color(),Color(),Color()  #BGP7
+            Color(0x0600),Color(0xfdf3),Color(0x041c),Color(0xf5db), #BGP0
+            Color(0x4419),Color(0x57ea),Color(0x2808),Color(0x9b75), #BGP1
+            Color(0x129b),Color(0xfce0),Color(0x22da),Color(0x4ac5), #BGP2
+            Color(0x2d71),Color(0xf0c2),Color(0x5137),Color(0x2d41), #BGP3
+            Color(0x6b2d),Color(0x2215),Color(0xbe0a),Color(0xc053), #BGP4
+            Color(0xfe5f),Color(0xe000),Color(0xbe10),Color(0x914d), #BGP5
+            Color(0x7f91),Color(0x02b5),Color(0x77ac),Color(0x14e5), #BGP6
+            Color(0xcf89),Color(0xa03d),Color(0xfd50),Color(0x91ff)  #BGP7
         ]
         self.bgpi = PaletteIndex()
 
@@ -138,9 +138,10 @@ class PaletteIndex:
 
 class Color:
 
-    def __init__(self):
+    def __init__(self, color = 0):
         self._value = 0
         self._rgba = 0
+        self.set_color(color)
 
     def set_low_byte(self, value):
         self._value = ((self._value & 0xff00) | value)
@@ -159,6 +160,10 @@ class Color:
     
     def get_rgba(self):
         return self._rgba
+
+    def set_color(self, color):
+        self.set_low_byte(color & 0xff)
+        self.set_high_byte(color >> 8)
 
 
     def color_5_5_5_to_rgba(self, color):
