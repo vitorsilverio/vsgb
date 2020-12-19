d4cfa7ad675eadc16e53a0806224a6b02d49fbb6
Vitor <vitor.silverio.rodrigues@gmail.com>
2020-12-19 18:34:27 -0300

    "FIX" Save state, actually reduce crashes, it's not fully functional

--- vsgb/save_state_manager.py
+++ vsgb/save_state_manager.py
@@ -12,13 +12,11 @@ class SaveStateManager:
     def create(self, emulator):
         temp_rom = emulator.mmu.rom.data
         with open(emulator.cartridge.rom().get_game_id()+'.bin','wb') as save_state_file:
-            self.timer = emulator.timer
             self.mmu = emulator.mmu
             self.mmu.rom.data = None
             self.ppu = emulator.ppu
             self.hdma = emulator.hdma
             self.dma = emulator.dma
-            self.interruptManager = emulator.interruptManager
             self.cpu = emulator.cpu
             pickle.dump(self, save_state_file)
         emulator.mmu.rom.data = temp_rom
@@ -28,14 +26,12 @@ class SaveStateManager:
         try:
             with open(emulator.cartridge.rom().get_game_id()+'.bin','rb') as save_state_file:
                 state = pickle.load(save_state_file)
-                emulator.timer = state.timer
                 emulator.mmu = state.mmu
                 emulator.mmu.rom.data = emulator.cartridge.rom().data
                 emulator.mmu.input = emulator.input
                 emulator.ppu = state.ppu
                 emulator.hdma = state.hdma
                 emulator.dma = state.dma
-                emulator.interruptManager = state.interruptManager
                 emulator.cpu = state.cpu
             logging.info('Loaded state')
         except:
